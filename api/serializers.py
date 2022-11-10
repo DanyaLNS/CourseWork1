0c0f4513330ebc4a7011b473cd63aa9c0f9864c9
DanyaLNS <danil-anikin-98@mail.ru>
2022-11-10 14:15:16 +0300

    Add documentation

--- api/serializers.py
+++ api/serializers.py
@@ -4,3 +4,4 @@ from rest_framework import serializers
 class ReaderSerializer(serializers.Serializer):
     path = serializers.CharField(max_length=300)
     result = serializers.CharField(max_length=50)
+
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+
 
 urlpatterns = [
-    path('<str:s>', views.ReaderView.as_view())
+    path('<str:s>', views.ReaderView.as_view()),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
 ]
